ccb826741c583ead67a4a9fd4c95eff635d584ae
Anna <annatran93@gmail.com>
2019-11-23 12:29:57 -0500

    bookmark array, scroll to top, convert price to dollar signs

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const cuisineApp = {};
 cuisineApp.key = '2a1fee6e3fbe95cb3651bb670e95b77e';
 
 // Cuisine Button Event Function controls what restaurants to search for based on cuisine button clicked
-cuisineApp.cuisineButtonEvent = function() {
-    
+cuisineApp.cuisineButtonEvent = function () {
+
     // Generate variable to store cuisine id 
     const cuisineButtons = Array.from($('#cuisineButtonsList').find('a'));
-    console.log(cuisineButtons);
+    // console.log(cuisineButtons);
 
-    cuisineButtons.forEach(function(button) {
-        $(button).on('click', function(){
-            if($('#displayedRestaurants li').length >= 1) {
+    cuisineButtons.forEach(function (button) {
+        $(button).on('click', function () {
+            if ($('#displayedRestaurants li').length >= 1) {
                 $('#displayedRestaurants').empty();
                 const cuisineID = $(button).attr('id');
                 cuisineApp.ajaxRequest(cuisineID);
@@ -27,30 +27,30 @@ cuisineApp.cuisineButtonEvent = function() {
     });
 }
 // AJAX Request Function determines what to search for based on user’s choice for cuisine
-cuisineApp.ajaxRequest = function(cID) {
-    
+cuisineApp.ajaxRequest = function (cID) {
+
     // AJAX Function will get info from URL based on argument passed
-    cuisineApp.restaurantOptions =  $.ajax({ 
-            url: `https://developers.zomato.com/api/v2.1/search?entity_id=89&entity_type=city&cuisines=${cID}`, 
-            method: 'GET', 
-            dataType: 'json', 
-            headers: {
-                'user-key': cuisineApp.key
-            }
-        }).then((result) => {
-            // Then store result in the variable generated
-            const { restaurants } = result;
-            // Call Display Restaurants Function passing the result variable as an argument
-            cuisineApp.displayRestaurants(restaurants);
-            cuisineApp.bookmarkRestaurants(restaurants);
-        }).catch((error) => {
-            console.log(error);
-        });
-    }
-    
+    cuisineApp.restaurantOptions = $.ajax({
+        url: `https://developers.zomato.com/api/v2.1/search?entity_id=89&entity_type=city&cuisines=${cID}`,
+        method: 'GET',
+        dataType: 'json',
+        headers: {
+            'user-key': cuisineApp.key
+        }
+    }).then((result) => {
+        // Then store result in the variable generated
+        const { restaurants } = result;
+        // Call Display Restaurants Function passing the result variable as an argument
+        cuisineApp.displayRestaurants(restaurants);
+        cuisineApp.bookmarkRestaurants(restaurants);
+    }).catch((error) => {
+        // console.log(error);
+    });
+}
+
 
 // Display Restaurants Function collected from AJAX request
-cuisineApp.displayRestaurants = function(item) {
+cuisineApp.displayRestaurants = function (item) {
     item.forEach(option => {
         const { restaurant } = option;
         // Pull keys from object for image, address, avg cost, phone number
@@ -85,54 +85,71 @@ cuisineApp.displayRestaurants = function(item) {
                         <h3>${name}</h3>
                         <p class="address">${address}</p>
                         <p>${phoneNumber}</p>
-                        <p>Price: ${priceRange} | ${reviewCount} Reviews</p>
+                        <p><span class="price">${priceRange}</span> | ${reviewCount} Reviews</p>
                     </div>
                 </div>
             </li>
-        `;    
-        $displayedRestaurants.append(restaurantDescription);         
+        `;
+        $displayedRestaurants.append(restaurantDescription);
+
+
+        //Function to replace represent price in dollar signs instead of number
+        const prices = $('.price');
+        const dollarSigns = '$'
+
+        const priceInDollars = $('.price').map(function () {
+            const priceInNumbers = parseInt($(this).text());
+            $(this).replaceWith(dollarSigns.repeat(priceInNumbers));
+        }) 
+
     });
 }
 cuisineApp.bookmarkRestaurants = function (item) {
-    
+
     const bookmarks = Array.from(document.querySelectorAll('.bookmark'));
     // returns an array of objects
-    console.log(bookmarks);
-    
-    const saved = [];
-    
+    // console.log(bookmarks);
+
+    let saved = [];
+
     bookmarks.forEach(function (bookmark) {
         $(bookmark).on('click', function () {
             if ($(this).hasClass('saved')) {
                 $(this).removeClass('saved');
-                checkForSameValue();
-            }
-            else {
+                saved = saved.filter(function (value) {
+                    return value !== $(bookmark).attr('id');
+                });
+            } else {
                 $(this).addClass('saved');
                 saved.push($(this).attr('id'));
             }
-            console.log(saved);
         })
-
-        const checkForSameValue = function() {
-            saved.filter(function(value) {
-                console.log(value);
-                // console.log($(bookmark).attr('id'));
-                return value == $(bookmark).attr('id');
-            });
-        }
-    });
-
+    })
 }
+
+//Click event to scroll to top
+$('.scrollToTop').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+});
+
+//Function to display scroll to top
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1300) {
+        $('.scrollToTop').fadeIn();
+    } else {
+        $('.scrollToTop').fadeOut();
+    }
+});
 
 
 // Start init app by calling cuisine button event function
-cuisineApp.init = function() {
+cuisineApp.init = function () {
     cuisineApp.cuisineButtonEvent();
 }
 
 // Create document ready to call app init function
-$(function() {
+$(function () {
     cuisineApp.init();
 });
 // SCRIPTS ENDS HERE
