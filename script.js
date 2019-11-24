2889ed586e5f37cc353456b0548c668e0c04fa6d
Anna <annatran93@gmail.com>
2019-11-24 13:15:39 -0500

    added login feature

--- script.js
+++ script.js
@@ -5,8 +5,159 @@ const cuisineApp = {};
 
 cuisineApp.key = '2a1fee6e3fbe95cb3651bb670e95b77e';
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDEs06ke1TV-dqwy1My_UuP848nVnJcAk0",
+    authDomain: "vince-and-anna-project-four.firebaseapp.com",
+    databaseURL: "https://vince-and-anna-project-four.firebaseio.com",
+    projectId: "vince-and-anna-project-four",
+    storageBucket: "vince-and-anna-project-four.appspot.com",
+    messagingSenderId: "451681928751",
+    appId: "1:451681928751:web:14bc6204b28039fb8468da"
+};
+
+
 // Cuisine Button Event Function controls what restaurants to search for based on cuisine button clicked
-cuisineApp.cuisineButtonEvent = function () {
+cuisineApp.buttonEvent = function () {
+    const $loginButton = $('#login');
+    const $signUpButton = $('#signUp');
+    const $signOutButton = $('#signOut');
+    const $closeModal = $('.close');
+    const $signUpModal = $('#signUpModal');
+    const $loginModal = $('#loginModal');
+    $signOutButton.on('click', function () {
+        firebase.auth().signOut().then(function () {
+            firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE)
+                .then(function () {
+                    const provider = new firebase.auth.GoogleAuthProvider();
+                    // In memory persistence will be applied to the signed in Google user
+                    // even though the persistence was set to 'none' and a page redirect
+                    // occurred.
+                    return firebase.auth().signInWithRedirect(provider);
+                })
+                .catch(function (error) {
+                    // Handle Errors here.
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                });
+            // Sign-out successful.
+        }).catch(function (error) {
+            // An error happened.
+            console.log(error);
+        });
+    });
+    $signUpButton.on('click', function () {
+        const $signUpForm = $('#signUpForm');
+        const $signUpEmailAddress = $('#signUpEmailAddress');
+        const $signUpPassword = $('#signUpPassword');
+        const $confirmPassword = $('#confirmPassword');
+        $loginModal.removeClass('showLoginModal');
+        $signUpModal.toggleClass('showSignUpModal');
+        $closeModal.on('click', function () {
+            $signUpModal.removeClass('showSignUpModal');
+        });
+        $signUpForm.on('submit', function (event) {
+            event.preventDefault();
+            if ((($signUpEmailAddress.val() !== "") && ($signUpPassword.val() !== "") && ($confirmPassword.val() !== "")) && ($signUpPassword.val() === $confirmPassword.val())) {
+                let email = $signUpEmailAddress.val();
+                let password = $signUpPassword.val();
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(function (error) {
+                        // Handle Errors here.
+                        const signUpErrorCode = error.code;
+                        const signUpErrorMessage = error.message;
+                        console.log(`${signUpErrorCode} ${signUpErrorMessage}`)
+                        // ...
+                    });
+                // const user = firebase.auth().currentUser;
+                // if (user) {
+                //     console.log('signed in');
+                //     $signOutButton.css('display', 'block');
+                //     $signUpButton.css('display', 'none');
+                //     $loginButton.css('display', 'none');
+                //     $signUpModal.removeClass('showSignUpModal');
+                //     // User is signed in.
+                // }
+                firebase.auth().onAuthStateChanged(function (user) {
+                    if (user) {
+                        console.log('signed in');
+                        $signOutButton.css('display', 'block');
+                        $signUpButton.css('display', 'none');
+                        $loginButton.css('display', 'none');
+                        $signUpModal.removeClass('showSignUpModal');
+                        // User is signed in.
+                    }
+                    else {
+                        console.log('signed out');
+                    }
+                });
+            }
+            else if ($signUpPassword.val() !== $confirmPassword.val()) {
+                alert('Error, your passwords do not match!');
+            }
+            else if (($signUpEmailAddress.val() === "") || ($signUpPassword.val() === "") || ($confirmPassword.val() === "")) {
+                alert('Error, you are missing an input field!');
+            }
+        });
+    });
+    $loginButton.on('click', function () {
+        const $loginForm = $('#loginForm');
+        let $loginEmailAddress = $('#loginEmailAddress');
+        let $loginPassword = $('#loginPassword');
+        $signUpModal.removeClass('showSignUpModal');
+        $loginModal.toggleClass('showLoginModal');
+        $closeModal.on('click', function () {
+            $loginModal.removeClass('showLoginModal');
+        });
+        $loginForm.on('submit', function (event) {
+            event.preventDefault();
+            if (($loginEmailAddress.val() !== "") && ($loginPassword.val() !== "")) {
+                let email = $loginEmailAddress.val();
+                let password = $loginPassword.val();
+                firebase.auth().signInWithEmailAndPassword(email, password)
+                    .catch(function (error) {
+                        // Handle Errors here.
+                        const loginErrorCode = error.code;
+                        const loginErrorMessage = error.message;
+                        // ...
+                        console.log(`${loginErrorCode} ${loginErrorMessage}`);
+                    });
+                firebase.auth().onAuthStateChanged(function (user) {
+                    if (user) {
+                        console.log('signed in');
+                        $signOutButton.css('display', 'block');
+                        $loginButton.css('display', 'none');
+                        $signUpButton.css('display', 'none');
+                        $loginModal.removeClass('showLoginModal');
+                        // User is signed out.
+                    }
+                    else {
+                        console.log('signed out');
+                    }
+                });
+                // const formData = {
+                //     email: $emailAddress.val(),
+                //     password: $password.val(),
+                // }
+                // usersRef.once('value', (data) => {
+                // const userData = data.val();
+                // if ((userData.email === formData.email) && (userData.password === formData.password)) {
+                //     alert('Success');
+                // }
+                // else if ((userData.email === formData.email) && (userData.password !== formData.password)) {
+                //     alert('Incorrect password');
+                // }
+                // else {
+                //     usersRef.push(formData);
+                //     // alert('Unsuccessful');
+                // }
+                // });
+            }
+            else if (($loginEmailAddress.val() === "") || ($loginPassword.val() === "")) {
+                alert('Error, you have entered an incorrect email address or password!');
+            }
+        });
+    });
 
     // Generate variable to store cuisine id 
     const cuisineButtons = Array.from($('#cuisineButtonsList').find('a'));
@@ -29,7 +180,7 @@ cuisineApp.cuisineButtonEvent = function () {
 }
 
 // SELECTING CUISINE VIA FORM ELEMENT
-cuisineApp.cuisineSelectEvent = function () {
+cuisineApp.selectEvent = function () {
 
     // Generate variable to store cuisine id 
     const cuisineForm = Array.from($('form').find('select'));
@@ -43,12 +194,12 @@ cuisineApp.cuisineSelectEvent = function () {
             if ($('#displayedRestaurants li').length >= 1) {
                 $('#displayedRestaurants').empty();
                 const cuisineID = option;
-                // console.log(cuisineID)
+                console.log(cuisineID)
                 cuisineApp.ajaxRequest(cuisineID);
             }
             else {
                 const cuisineID = option;
-                // console.log(cuisineID)
+                console.log(cuisineID)
                 cuisineApp.ajaxRequest(cuisineID);
             }
         });
@@ -189,8 +340,10 @@ $(window).scroll(function () {
 
 // Start init app by calling cuisine button event function
 cuisineApp.init = function () {
-    cuisineApp.cuisineButtonEvent();
-    cuisineApp.cuisineSelectEvent();
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    cuisineApp.buttonEvent();
+    cuisineApp.selectEvent();
 }
 
 // Create document ready to call app init function
